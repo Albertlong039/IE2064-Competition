37aea9b0d2188c817fb781cdde83bc001e2331a7
Albertlong039 <92559960+Albertlong039@users.noreply.github.com>
2023-03-22 20:21:07 -0400

    Update dataprocesssing.R

--- dataprocesssing.R
+++ dataprocesssing.R
@@ -13,19 +13,4 @@ data1 <- read.csv(
   paste(cwd,"/data/2022 Inpatient discharges total.csv",sep=""),
   stringsAsFactors = FALSE
 )
-summary(data)
 
-data <- data %>%
-  group_by(EMPI) %>%
-  mutate(avgLOS = mean(Length.of.Staying))
-
-
-histogram <- hist(data$Length.of.Staying, breaks = 100, plot = FALSE)
-
-# Calculate the number of observations in each group
-group_counts <- histogram$counts
-
-# Create a new variable to store the data of groups larger than 10
-large_groups <- group_counts[group_counts > 10]
-
-# Create a histogram of the large groups
